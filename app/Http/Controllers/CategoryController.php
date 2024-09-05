b8ca7fa4a9955dda0210591895bac4a48796639d
Mariano <marianofrank@hotmail.com>
2024-09-05 16:20:39 -0300

    050924

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,14 +2,72 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\CategoryCollection;
 use App\Models\Category;
+use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
     public function index()
     {
         //esto no hace falta porque laravel serializa las colecciones y modelos automaticamente a JSON
-       // return response()->json(Category::all());
-       return Category::all();
+        // return response()->json(Category::all());
+
+        //simplemente con esto ya estariamos retornano un JSON a la respuesta
+        //return Category::all(); 
+
+        //pero vamos a usar resoruces
+        return new CategoryCollection(Category::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
